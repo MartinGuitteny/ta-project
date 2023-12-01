40e93f84bb1be60ebdbf867403d967498f22b50d
Martin Guitteny <martin.guitteny@telecomnancy.eu>
2023-12-01 01:33:28 -0500

    avancement méthode SVC

---
+++ src/SVC.py
@@ -0,0 +1,31 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn import svm
+
+class SVC:
+    def __init__(self,C=1,gam=0.1,ker='rbf'):
+        self.C = C
+        self.kernel = ker
+        self.gamma = gam
+
+        self.model = svm.SVC(C = self.C,kernel = self.kernel,gamma = self.gamma)
+
+    def entrainement(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def prediction(self, X_test):
+        return self.model.predict(X_test)
+
+    def recherche_parametres(self, X, t, cv=10):
+        param_grid = {'C': [0.001,0.01,0.1, 1,10], 'kernel': ['linear', 'rbf','sigmoid','poly'], 'gamma': [0.001,0.01,0.1, 1]}
+        grid_search = GridSearchCV(self.model, param_grid, cv=cv)
+        grid_search.fit(X, t)
+        self.C = grid_search.best_params_.get("C")
+        self.kernel = grid_search.best_params_.get("kernel")
+        self.gamma = grid_search.best_params_.get("gamma")
+        self.model = grid_search.best_estimator_
+    
+    def afficher_parametres(self):
+        print(f'''Parametres:
+C :{self.C}
+kernel : {self.kernel}
+gamma : {self.gamma}''')
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import gestion_donnees as gd
+import SVC as SV
 
 def main():
     donnees = gd.GestionDonnees("./data/train.csv")
     datas=donnees.transformation("species")
     X_train,X_test,t_train,t_test = gd.separation(datas)
-
+    svc = SV.SVC()
+    svc.recherche_parametres(X_train,t_train)
+    svc.afficher_parametres()
+    svc.entrainement(X_train,t_train)
 
 if __name__ == "__main__":
     main()
