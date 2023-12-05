026d35b7bfaf2904012a2672677aedc0f9174230
Matéo <mdemangeon2@gmail.com>
2023-12-05 18:07:12 -0500

    perceptron multi-couches

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import gestion_donnees as gd
-import SVC as SVC
+import SVC
 import KNN
+import Perceptron
 from sklearn.metrics import recall_score, precision_score
 
 
@@ -10,18 +11,19 @@ def main():
     X_train,X_test,t_train,t_test = gd.separation(datas)
     svc = SVC.SVC()
     knn = KNN.KNN()
-    methodes = [svc,knn]
+    perceptron = Perceptron.MLPModel()
+    methodes = [svc,knn,perceptron]
     for methode in methodes:
         methode.recherche_parametres(X_train,t_train)
         methode.afficher_parametres()
         methode.entrainement(X_train,t_train)
         pred = methode.prediction(X_test)
         precision = precision_score(t_test,pred,average='macro')
-        recall = recall_score(t_test,pred,average='macro')
-        print(f'''Parametres:
-modele :{methode.name()}
-precision : {precision}
-recall : {recall}''')
+        recall = recall_score(t_test,pred,average='macro', zero_division = 0)
+        print(f'''Paramètres :
+Modèle : {methode.name()}
+Precision : {precision}
+Recall : {recall}''')
 
 if __name__ == "__main__":
     main()
