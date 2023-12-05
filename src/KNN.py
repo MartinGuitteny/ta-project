ed1fed351d86af6d88ab741c83bde41e3b133d57
Martin Guitteny <martin.guitteny@telecomnancy.eu>
2023-12-05 03:54:05 -0500

    adding KNN

---
+++ src/KNN.py
@@ -0,0 +1,26 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+
+class KNN:
+    def __init__(self,n=5):
+        self.n=n
+        self.model = KNeighborsClassifier(n_neighbors=self.n)
+
+    def entrainement(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def prediction(self, X_test):
+        return self.model.predict(X_test)
+
+    def recherche_parametres(self, X, t, cv=10):
+        param_grid = {'n_neighbors': [5,7,10,12,15]}
+        grid_search = GridSearchCV(self.model, param_grid, cv=cv)
+        grid_search.fit(X, t)
+        self.C = grid_search.best_params_.get("C")
+        self.kernel = grid_search.best_params_.get("kernel")
+        self.gamma = grid_search.best_params_.get("gamma")
+        self.model = grid_search.best_estimator_
+    
+    def afficher_parametres(self):
+        print(f'''Parametres:
+n :{self.n}''')
--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import gestion_donnees as gd
-import SVC as SV
+import SVC as SVC
+import KNN
 
 def main():
     donnees = gd.GestionDonnees("./data/train.csv")
     datas=donnees.transformation("species")
     X_train,X_test,t_train,t_test = gd.separation(datas)
-    svc = SV.SVC()
-    svc.recherche_parametres(X_train,t_train)
-    svc.afficher_parametres()
-    svc.entrainement(X_train,t_train)
+    # svc = SVC.SVC()
+    # svc.recherche_parametres(X_train,t_train)
+    # svc.afficher_parametres()
+    # svc.entrainement(X_train,t_train)
+    knn = KNN.KNN()
+    knn.recherche_parametres(X_train,t_train)
+    knn.afficher_parametres()
+    knn.entrainement(X_train,t_train)
 
 if __name__ == "__main__":
     main()
