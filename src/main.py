01dbda5af0d9f9eec756cbed18220774e2567ce4
Matéo Demangeon <mdemangeon2@gmail.com>
2023-12-11 15:08:29 -0500

    perceptron simple

--- src/main.py
+++ src/main.py
@@ -6,29 +6,33 @@ import modele_generatif
 import logistic
 import evaluation_metrics
 import GBT
+import perceptron
+
 
 def main():
     donnees = gd.GestionDonnees("./data/train.csv")
-    datas=donnees.transformation("species")
-    X_train,X_test,t_train,t_test = gd.separation(datas)
+    datas = donnees.transformation("species")
+    X_train, X_test, t_train, t_test = gd.separation(datas)
 
     generatif = modele_generatif.ModeleGeneratif()
     svc = SVC.SVC()
     knn = KNN.KNN()
     reseau_neurones = reseau_de_neurones.ReseauDeNeurones()
     logistic_ = logistic.Logistic()
-    tree= GBT.GBT()
+    tree = GBT.GBT()
+    perceptron_ = perceptron.PerceptronModel()
 
-    methodes = [svc, knn, reseau_neurones, generatif, logistic_,tree]
+    methodes = [svc, knn, reseau_neurones, generatif, logistic_, tree, perceptron_]
 
     ev = evaluation_metrics.evaluation_metrics(t_test)
 
     for methode in methodes:
-        methode.recherche_parametres(X_train,t_train)
+        methode.recherche_parametres(X_train, t_train)
         methode.afficher_parametres()
-        methode.entrainement(X_train,t_train)
+        methode.entrainement(X_train, t_train)
         pred = methode.prediction(X_test)
-        ev.evaluate(methode,pred)
+        ev.evaluate(methode, pred)
+
 
 if __name__ == "__main__":
     main()
